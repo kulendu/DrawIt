57282baf0c64b83ff25c9886b2c094f56138bb58
kulendu <imkulendu@gmail.com>
2020-12-24 23:31:09 +0530

    Adding the LICENSE.md file

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ red_upper_bound = np.array([20,255,255])
 #   defining the color palette
 color_list = [(255, 0, 0), (0, 0, 255), (0, 255, 0), (0, 255, 255), (255, 255, 255)]
 color_palette_colors = [(255,0,0), (0,0,255)]
+
 # index for the colors in our palette
 color_index = 0
+
+
+
