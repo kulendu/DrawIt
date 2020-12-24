46fd9cebe548f8114f92afd75c68dcc2510b12c3
kulendu <imkulendu@gmail.com>
2020-12-24 23:23:36 +0530

    getting started

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import cv2
+import  numpy as np
 from collections import deque
 
+#   using the red color as the marker
+red_lower_bound = np.array([0,100,100])
+red_upper_bound = np.array([20,255,255])
+
+#   defining the color palette
+color_list = [(255, 0, 0), (0, 0, 255), (0, 255, 0), (0, 255, 255), (255, 255, 255)]
+color_palette_colors = [(255,0,0), (0,0,255)]
+# index for the colors in our palette
+color_index = 0
